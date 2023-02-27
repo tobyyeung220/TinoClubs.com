ceb793de65da0021d416450566f1a52e5f941525
iamjiamingliu <jiamingliu888@gmail.com>
2023-02-26 16:23:22 -0800

    saving raw leadership info

--- load_club_data.py
+++ load_club_data.py
@@ -1,35 +1,34 @@
-# PUTS THIS IN main.py
+# PUT THIS IN main.py
 
 import json
 import os
 from db import Club
 
-categories_mapping = {
-    'volunteering & honor societies': ClubCategory.volunteering,
-    'hobbies & interest': ClubCategory.hobbies,
-    'culture & ethnicities': ClubCategory.culture_and_identity,
-    'other': ClubCategory.culture_and_identity,
-    'sports & athletics': ClubCategory.sports,
-    'stem & tech': ClubCategory.stem,
-    'business & politics': ClubCategory.business,
-    'culture (or activist society)': ClubCategory.culture_and_identity,
-    'volunteering&honor societies': ClubCategory.hobbies,
-    'volunteering/service club': ClubCategory.volunteering,
-    'stem & tech, student publication': ClubCategory.stem,
-    'interest club': ClubCategory.hobbies,
-    'other (activist society)': ClubCategory.culture_and_identity
-}
 
-for file in os.listdir("JSONs"):
-    club_data = json.load(open(os.path.join("JSONs", file), 'r'))
-    category = club_data['category'].replace('and', '&').replace('interests', 'interest')
-    with app.app_context():
-        try:
-            Club.create(name=club_data['name'], aka='', category=categories_mapping[category], description='',
-                        meeting_time=club_data['meeting_time'], meeting_location=club_data['advisor_room'], raw_tags='',
-                        raw_social_medias=json.dumps(
-                            [{'name': 'email', 'url': 'mailto:' + club_data['email'], 'text': club_data['email']}]),
-                        raw_leaderships=json.dumps([{'name': club_data['advisor_name'], 'role': 'Advisor'}]),
-                        is_new=False)
-        except Exception as e:
-            print(e, club_data)
+def keep_only_ascii(text: str) -> str:
+    return ''.join(char for char in text if char.isalpha() or char == ' ').strip()
+
+
+def load_club_officers(club_leadership_text):
+    acceptable_roles = ['president', 'vice', 'vp', 'secretary', 'treasurer']
+    return [{'name': keep_only_ascii(line.split(',')[0]), 'role': line.split(',')[2].strip()} for line in club_leadership.split('\n') if len(line.split(',')) >= 3 and any(r in line.split(',')[2].strip().lower() for r in acceptable_roles)]
+
+
+for club_data_json in os.listdir('JSONs'):
+    club_data = json.load(open(os.path.join('JSONs', club_data_json), 'r'))
+    club_leadership = club_data['officers']
+    print()
+    # print('-' * 100)
+    print(club_data['name'])
+    print(club_leadership)
+    officer_info = load_club_officers(club_leadership)
+    print(officer_info)
+    if (record := input('y to ok or enter custom data: ')) != 'y':
+        while 1:
+            try:
+                officer_info = json.loads(record)
+                break
+            except:
+                record = input('invalid json, try again: ')
+    with open('test.txt', 'a') as file:
+        file.write(club_data['name'] + ' ' + json.dumps(officer_info) + '\n')
